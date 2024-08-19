2c4c84148569d6b60ae9118946982266d71c5733
Attent1 <rafaeldsc22@outlook.com>
2024-08-19 09:40:52 -0300

    added login with token JWT

---
+++ src/main/java/br/com/fiap/apisphere/auth/TokenService.java
@@ -0,0 +1,27 @@
+package br.com.fiap.apisphere.auth;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    public Token createToken(String email) {
+        var expirationAt = LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.UTC);
+        Algorithm algorithm = Algorithm.HMAC256("assinatura");
+        String token = JWT.create()
+                .withSubject(email)
+                .withExpiresAt(expirationAt)
+                .withIssuer("sphere")
+                .withClaim("role", "ADMIN")
+                .sign(algorithm);
+
+        return new Token(token, email);
+
+    }
+
+}
