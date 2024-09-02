c95f4bfb086c156537c51d21ffd487a3a5834070
Attent1 <rafaeldsc22@outlook.com>
2024-09-02 09:47:20 -0300

    file upload

--- src/main/java/br/com/fiap/apisphere/user/UserController.java
+++ src/main/java/br/com/fiap/apisphere/user/UserController.java
@@ -8,8 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
@@ -44,4 +49,33 @@ public class UserController {
         return service.getProfile(email);
     }
 
+    @PostMapping("avatar")
+    public void uploadAvatar(@RequestBody MultipartFile file){
+        // receber a foto
+
+        // verificar se o arquivo existe
+        if(file.isEmpty()){
+            throw new RuntimeException("Archive cannot be empty");
+        }
+
+        var email = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+
+        Path destinationRoot = Path.of("src/main/resources/static/avatars/");
+        Path destinationFile = destinationRoot.resolve(
+                Paths.get(System.currentTimeMillis() + email + file.getOriginalFilename())
+        )
+                .normalize()
+                .toAbsolutePath();
+
+        try(InputStream is = file.getInputStream()) {
+            Files.copy(is, destinationFile);
+            System.out.println("File copied" );
+
+            var avatarURL = "http://localhost:8080/avatars/" + file.getOriginalFilename();
+            service.updateAvatar(email, avatarURL);
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
